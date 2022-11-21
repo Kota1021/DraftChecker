eba2a7f2d3218275657a4938b93c221bf0c2b46d
Kotaro Matsumoto <km0813832@gmail.com>
2022-11-21 18:02:22 +0900

    Initial Commit

---
+++ DraftChecker/DraftCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraftCheckerApp.swift
+//  DraftChecker
+//
+//  Created by 松本幸太郎 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DraftCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
